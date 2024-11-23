a705a52ca1e9d27d5d8ae8d5a6c0de67363d3b02
vaibhav vemani <vaibhavvemani@gmail.com>
2024-11-23 13:17:06 +0530

    search component complete

--- src/app/[car]/page.js
+++ src/app/[car]/page.js
@@ -1,3 +1,4 @@
+'use server'
 import React from 'react'
 import CarSearch from '@/components/CarSearch'
 const page = () => {
--- src/components/CarSearch.jsx
+++ src/components/CarSearch.jsx
@@ -1,15 +1,42 @@
+'use client'
 import React from 'react'
 import { createClient } from '@/utils/supabase/client'
+import { useState, useEffect} from 'react'
 
+export default function CarSearch() {
+    const [search, setSearch] = useState('')
+    const [cars, setData] = useState([])
+    const [filteredCars, setFilteredCars] = useState([])
 
-export default async function CarSearch() {
-    const {data, error} = await createClient()
-        .from('testloader')
-        .select()
+    useEffect(() => {
+        async function fetchData() {
+            const {data, error} = await createClient()
+                .from('testloader')
+                .select('car_name')
+            setData(data)
+        }
+        fetchData()
+    }, [])
+
+    useEffect(() => {
+        if (search.trim() === '') {
+            setFilteredCars([]);
+        } else {
+            const results = cars.filter((car) =>
+                car.car_name.toLowerCase().includes(search.toLowerCase())
+            );
+            setFilteredCars(results);
+        }
+    }, [search, cars]);
 
     return (
         <div>
-            <input type="text" placeholder="Search for a car" />
+            <input type="text" placeholder="Search for a car" value={search} onChange={(e) => {setSearch(e.target.value)}}/>
+            <ul>
+                {filteredCars.map((car) => (
+                    <li key={car.id} onClick={() => alert(`You selected ${car.car_name}`)}>{car.car_name}</li>
+                ))}
+            </ul>
         </div>
     )
 }
