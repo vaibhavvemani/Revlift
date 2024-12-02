914d0ed531bf5066b3cda5dea92d8adc7f060bc5
vaibhav vemani <vaibhavvemani@gmail.com>
2024-12-02 13:21:35 +0530

    working on displaying the car details

--- src/components/CarDisplay.jsx
+++ src/components/CarDisplay.jsx
@@ -2,34 +2,38 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { createClient } from '@/utils/supabase/client'
-
+import Image from 'next/image'
+import "@/styles/cardisplay.css"
 export const CarDisplay = ( props ) => {
   const [data, setData] = useState([])
 
   useEffect(() => {
     async function fetchData() {
-      // const car = await props.car
-      const { data: fetchedData, error } = await createClient()
+      const { data, error } = await createClient()
         .from('cars')
         .select()
         .eq('name', props.car)
-      setData(fetchedData)
+      setData(data)
+      console.log(data)
     }
     fetchData()
   }, [props.car])
-
+  
+  let car = data[0]
 
   return (
-    <>
-      <ul>
-        {data.map((car, index) => (
-          <li key={index}>
-            <h2>{car.name}</h2>
-            <p>{car.description}</p>
-            <img src={car.car_image} alt={car.name} />
-          </li>
-        ))}
-      </ul>
-    </>
+    <div className="display-container">
+      <h1>{car?.name}</h1>
+      <div className="left-container">
+        <Image className='car-image' src="/911_GT3_RS.jpg" alt={car?.name} width={720} height={450} />
+        <p className='description'>{car?.description}</p>
+      </div>
+      <div className="right-container">
+        <h2>Price: {car?.price}$</h2>
+        <section className='power-info'></section>
+        <section className='speed-info'></section>
+        <section className='dimention-info'></section>
+      </div>
+    </div>
   )
 }
